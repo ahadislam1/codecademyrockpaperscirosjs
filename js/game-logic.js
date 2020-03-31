e05ac90b04d9b3310df377ef9b03ce54bf62a3f4
Ahad Islam <ahad.islam624@gmail.com>
2020-03-31 10:20:53 -0400

    solved the getplayerthing

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,20 +1,84 @@
 // All code should be written in this file.
-let playerOneMoveOneType
-let playerOneMoveTwoType
-let playerOneMoveThreeType
+let playerOneMoveOneType;
+let playerOneMoveTwoType;
+let playerOneMoveThreeType;
 
-let playerOneMoveOneValue
-let playerOneMoveTwoValue
-let playerOneMoveThreeValue
+let playerOneMoveOneValue;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeValue;
 
-let playerTwoMoveOneType
-let playerTwoMoveTwoType
-let playerTwoMoveThreeType
+let playerTwoMoveOneType;
+let playerTwoMoveTwoType;
+let playerTwoMoveThreeType;
 
-let playerTwoMoveOneValue
-let playerTwoMoveTwoValue
-let playerTwoMoveThreeValue
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeValue;
 
-function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
-    
+function setPlayerMoves(
+  player,
+  moveOneType,
+  moveOneValue,
+  moveTwoType,
+  moveTwoValue,
+  moveThreeType,
+  moveThreeValue
+) {
+  let x = [
+    moveOneType,
+    moveOneValue,
+    moveTwoType,
+    moveTwoValue,
+    moveThreeType,
+    moveThreeValue
+  ];
+
+  let validMoveTypes = ['rock', 'paper', 'scissors']
+  let values = [moveOneValue, moveTwoValue, moveThreeValue]
+
+  function checkValues() {
+    let sum = 0
+
+    for (value of values) {
+      if (value < 1 || value > 99 ) {
+        return false
+      }
+      sum += value
+    }
+
+    if (sum > 99 ){
+      return false
+    }
+
+    return true
+  }
+
+
+  const checkMoveValid = moveType => validMoveTypes.includes(moveType)
+
+  if (!x.includes(undefined) && checkMoveValid(moveOneType) && checkMoveValid(moveTwoType) && checkMoveValid(moveThreeType) && checkValues()) {
+    if (player === "Player One") {
+      playerOneMoveOneType = moveOneType;
+      playerOneMoveOneValue = moveOneValue;
+
+      playerOneMoveTwoType = moveTwoType;
+      playerOneMoveTwoValue = moveTwoValue;
+
+      playerOneMoveThreeType = moveThreeType;
+      playerOneMoveThreeValue = moveThreeValue;
+    } else if (player === "Player Two") {
+      playerTwoMoveOneType = moveOneType;
+      playerTwoMoveOneValue = moveOneValue;
+
+      playerTwoMoveTwoType = moveTwoType;
+      playerTwoMoveTwoValue = moveTwoValue;
+
+      playerTwoMoveThreeType = moveThreeType;
+      playerTwoMoveThreeValue = moveThreeValue;
+    }
+  }
 }
+
+function getRoundWinner() { }
+
+function getGameWinner() { }
